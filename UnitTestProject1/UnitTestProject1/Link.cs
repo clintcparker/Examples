4228c04ebad46ae5abedbfc300e373715467eed9
Clint Parker <clintcparker@gmail.com>
2016-02-08 16:58:23 -0800

    added files

---
+++ UnitTestProject1/UnitTestProject1/Link.cs
@@ -0,0 +1,9 @@
+﻿namespace UnitTestProject1
+{
+    public class Link
+    {
+        public LinkType type { get; set; }
+        public string url { get; set; }
+        public string suggested_link_text { get; set; }
+    }
+}
---
+++ UnitTestProject1/UnitTestProject1/LinkType.cs
@@ -0,0 +1,12 @@
+namespace UnitTestProject1
+{
+    public enum LinkType
+    {
+        article,
+        overview,
+        showtimes,
+        awards,
+        community,
+        trailers
+    }
+}
